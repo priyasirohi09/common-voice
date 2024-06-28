239cb86bccbf043c9aa87daab65e60dfd82a7b75
kohantikanath <kohan.nath@gmail.com>
2024-06-28 12:26:13 +0530

    Added hover effect

--- web/src/components/contact-modal/contact-modal.tsx
+++ web/src/components/contact-modal/contact-modal.tsx
@@ -23,15 +23,15 @@ export default function ContactModal({ onRequestClose }: Props) {
       <br />
 
       <Localized id="email-input" attrs={{ label: true }}>
-        <LabeledInput label="Email" name="email" required type="email" placeholder="Enter your email"/>
+        <LabeledInput label="Email" name="email" required type="email"/>
       </Localized>
 
       <Localized id="contact-form-name" attrs={{ label: true }}>
-        <LabeledInput label="Name" name="name" type="text" placeholder="Enter your name"/>
+        <LabeledInput label="Name" name="name" type="text"/>
       </Localized>
 
       <Localized id="contact-form-message" attrs={{ label: true }}>
-        <LabeledTextArea label="Message" name="message" required rows={6} placeholder="Enter your message"/>
+        <LabeledTextArea label="Message" name="message" required rows={6}/>
       </Localized>
 
         <div className="actions">
